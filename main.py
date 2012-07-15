5fe72bce3ea71764c30f9c78c502339121413830
ReanGD <ReanGD@yandex.ru>
2012-07-15 20:54:55 +0400

    Issue #6 Реализов выбор слов для изучения на основании статистики

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+import math
 import json
 import random
 import datetime
@@ -12,13 +13,44 @@ ru_to_en_write = 1
 
 class Statistic:
 	def __init__(self):
-		self.success_answer     = 0
-		self.error_answer       = 0
-		self.last_lesson_time   = None
+		self.success_answer     = 0.0
+		self.error_answer       = 0.0
+		self.last_lesson_date   = None
 		self.last_lesson_result = None
 
+	def get_total_answer(self):
+		return self.success_answer+self.error_answer
+
+	def get_success_persent(self):
+		total = self.get_total_answer()
+		if total > 0:
+			return float(self.success_answer)/total*100.0
+		else:
+			return 0.0
+
+	def calc_rating(self, min_percent, min_success_cnt):
+		pers  = self.get_success_persent()
+		total = self.get_total_answer()
+		
+		rating = 100.0 - pers
+		# для изученных слов уменьшаем рейтинг
+		if pers >= min_percent and total >= min_success_cnt:
+			rating /= 3.0
+		# чем чаще слово повторяли, тем меньше рейтинг
+		rating *= math.exp(-total*0.07)
+		# если последний ответ был неправильным увеличиваем рейтинг
+		if self.last_lesson_result == False:
+			rating *= 1.5
+		# чем дольше слово не повторяли, тем выше рейтинг
+		days = 0
+		if self.last_lesson_date != None:
+			days = (datetime.date.today() - datetime.datetime.strptime(self.last_lesson_date, "%Y.%m.%d").date()).days
+		rating *= math.log10(days+1.0)+1.0
+
+		return rating
+
 	def update(self, is_success, dt):
-		self.last_lesson_time   = dt
+		self.last_lesson_date   = dt
 		self.last_lesson_result = is_success
 		if is_success:
 			self.success_answer += 1
@@ -26,10 +58,10 @@ class Statistic:
 			self.error_answer += 1
 
 	def unpack(self, statistic):
-		self.success_answer, self.error_answer, self.last_lesson_time, self.last_lesson_result = statistic
+		self.success_answer, self.error_answer, self.last_lesson_date, self.last_lesson_result = statistic
 
 	def pack(self):
-		return [self.success_answer, self.error_answer, self.last_lesson_time, self.last_lesson_result]
+		return [self.success_answer, self.error_answer, self.last_lesson_date, self.last_lesson_result]
 
 class Word:
 	def __init__(self):
@@ -37,6 +69,7 @@ class Word:
 		self.transcription	= ""
 		self.ru_word		= ""
 		self.ru_word_list	= []
+		self.rating			= 0
 		self.stat 			= {en_to_ru_write : Statistic(), ru_to_en_write : Statistic()}
 
 	def add_value(self, en_word, transcription, ru_word):
@@ -49,6 +82,12 @@ class Word:
 		else:
 			self.ru_word		= "%s, %s" % (self.ru_word, ru_word.strip())
 		self.ru_word_list		= map(lambda x : x.strip().lower(), self.ru_word.split(","))
+
+	def set_rating(self, value):
+		self.rating = value
+
+	def get_rating(self):
+		return self.rating
 
 	def source_data(self, type_pr):
 		if type_pr == en_to_ru_write:
@@ -68,6 +107,12 @@ class Word:
 	def update_stat(self, is_success, dt, type_pr):
 		self.stat[type_pr].update(is_success, dt)
 
+	def is_load(self):
+		return self.transcription != ""
+
+	def get_stat(self, type_pr):
+		return self.stat[type_pr]
+
 	def unpack(self, statistic):
 		for it in statistic:
 			it_int = int(it)
@@ -84,6 +129,7 @@ class Word:
 class Dict:
 	def __init__(self):
 		self.words = {}
+		self.lesson_words = []
 
 	def get_word_by_key(self, en):
 		word = self.words.get(en)
@@ -107,6 +153,53 @@ class Dict:
 			for it in data:
 				self.get_word_by_key(it).unpack(data[it])
 
+	def loaded_words(self, type_pr):
+		return [(it, it.get_stat(type_pr)) for it in self.words.values() if it.is_load()]
+
+	def words_for_lesson(self, cnt_study_words, min_percent, min_success_cnt, type_pr):
+		learned_words = []
+		studied_words = []
+		for word, stat in self.loaded_words(type_pr):
+			if stat.get_total_answer() > 0:
+				if stat.get_success_persent() >= min_percent and stat.get_total_answer() >= min_success_cnt:
+					learned_words.append(word)
+				else:
+					studied_words.append(word)
+
+		# дополняем изучаемыми/изученными словами из другого направления перевода
+		if len(studied_words) < cnt_study_words:
+			inv_type_pr = ru_to_en_write if type_pr == en_to_ru_write else en_to_ru_write
+			for word, stat in self.loaded_words(inv_type_pr):
+				if stat.get_total_answer() > 0 and word not in (learned_words+studied_words):
+					studied_words.append(word)
+					if len(studied_words) == cnt_study_words:
+						break
+
+		# дополняем ни разу не изучаемыми словами
+		if len(studied_words) < cnt_study_words:
+			for word, stat in self.loaded_words(type_pr):
+				if stat.get_total_answer() == 0:
+					studied_words.append(word)
+					if len(studied_words) == cnt_study_words:
+						break
+
+		studied_words.sort(key = lambda it : it.get_stat(type_pr).get_success_persent())
+		studied_words = studied_words[:cnt_study_words]
+
+		return learned_words + studied_words
+
+	def calc_word_scores(self, cnt_study_words, min_percent, min_success_cnt, type_pr):
+		self.lesson_words = self.words_for_lesson(cnt_study_words, min_percent, min_success_cnt, type_pr)
+		
+		for it in self.lesson_words:
+			rating = it.get_stat(type_pr).calc_rating(min_percent, min_success_cnt)
+			it.set_rating(rating)
+
+		#normalize		
+		max_rating = max([it.get_rating() for it in self.lesson_words])
+		for it in self.lesson_words:
+			it.set_rating(it.get_rating()/max_rating)
+
 	def save_stat(self, path_to_stat):
 		data = {}
 		for it in self.words:
@@ -115,7 +208,10 @@ class Dict:
 		json.dump(stat_json, open(path_to_stat, "wb"))
 
 	def get_any_word(self):
-		return random.choice(self.words.values())
+		while True:
+			word = random.choice(self.lesson_words)
+			if word.get_rating() > random.random():
+				return word
 
 class Practice:
 	def __init__(self, lesson, word, type_pr):
@@ -137,16 +233,18 @@ class Practice:
 		return is_success, right_answer, right_answer_tr
 
 class Lesson:
-	def __init__(self, path_to_dict, path_to_stat, max_success):
+	def __init__(self, cfg):
+		random.seed()
 		self.type_pr       = random.choice([en_to_ru_write, ru_to_en_write])
 		self.dict          = Dict()
-		self.max_success   = max_success
+		self.max_success   = cfg["words_per_lesson"]
 		self.cnt_success   = 0
 		self.cnt_error     = 0
-		self.path_to_stat  = path_to_stat
+		self.path_to_stat  = cfg["path_to_stat"]
 		self.practice_list = []
-		self.dict.reload_dict(path_to_dict)
-		self.dict.reload_stat(path_to_stat)
+		self.dict.reload_dict(cfg["path_to_dict"])
+		self.dict.reload_stat(cfg["path_to_stat"])
+		self.dict.calc_word_scores(cfg["CntStudyWords"], cfg["MinPercent"], cfg["MinSuccessCnt"], self.type_pr)
 
 	def update_stat(self, is_success):
 		if is_success:
@@ -157,7 +255,7 @@ class Lesson:
 	def end_lesson(self):
 		self.dict.reload_stat(self.path_to_stat)
 
-		dt = datetime.datetime.now().strftime("%Y%m%dT%H:%M:%S")
+		dt = datetime.date.today().strftime("%Y.%m.%d")
 		for it in self.practice_list:
 			it.update_stat(dt)
 
@@ -189,7 +287,7 @@ class App:
 	def new_lesson(self):		
 		cfg             = self.load_config("config.json")
 		self.retry_time = cfg["retry_time"]
-		self.lesson     = Lesson(cfg["path_to_dict"], cfg["path_to_stat"], cfg["words_per_lesson"])
+		self.lesson     = Lesson(cfg)
 		self.get_next_practice()
 		self.win.show()
 
