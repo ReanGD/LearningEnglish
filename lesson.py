b063d30b0d8b3442e2e542af65a78b0e178e9c6d
ReanGD <ReanGD@yandex.ru>
2012-08-16 00:32:33 +0400

    Issue #21 Поправил ошибку с обновлением статистики при досрочном завершении программы

--- lesson.py
+++ lesson.py
@@ -10,13 +10,14 @@ class Practice:
 		self.lesson  = lesson
 		self.word    = word
 		self.type_pr = type_pr
-		self.result  = False
+		self.result  = None
 
 	def source_data(self):
 		return self.word.source_data(self.type_pr)
 
 	def update_stat(self, dt):
-		self.word.update_stat(self.result, dt, self.type_pr)
+		if self.result != None:
+			self.word.update_stat(self.result, dt, self.type_pr)
 
 	def check(self, user_answer):
 		is_success, right_answer = self.word.check(user_answer, self.type_pr)
